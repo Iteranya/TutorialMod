b9e0b264df679db5d602382da1f37d6f0c7608a4
Iteranya <pandudparadox@gmail.com>
2023-12-02 22:07:00 +0700

    I have successfully created the BEEG SWORD
    It hurts a lot
    
    And it also heals you whenever you swing it
    
    ...
    
    It's broken OP~

--- src/main/java/net/iteranya/tutorialmod/item/custom/BeegSwordItem.java
+++ src/main/java/net/iteranya/tutorialmod/item/custom/BeegSwordItem.java
@@ -1,14 +1,9 @@
 package net.iteranya.tutorialmod.item.custom;
 
-import net.minecraft.world.InteractionHand;
-import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.player.Player;
-import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.SwordItem;
 import net.minecraft.world.item.Tier;
-import net.minecraft.world.level.Level;
 
 public class BeegSwordItem extends SwordItem {
     public BeegSwordItem(Tier tier, int damage, float speed, Properties properties) {
